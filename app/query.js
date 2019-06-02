7dca98ce44b9e1a5c80bfe1d1b74212fb4773048
Aviel Cana <acana@stratpoint.com>
2019-06-02 21:37:10 +0800

    change query

--- app/query.js
+++ app/query.js
@@ -14,7 +14,7 @@ pool.connect((err, client, release) => {
 })
 
 const getContacts = (request, response) => {
-  pool.query('SELECT * FROM contacts', (error, results) => {
+  pool.query('SELECT * FROM contactsdb', (error, results) => {
     if(error) {
       throw error
     }
@@ -25,7 +25,7 @@ const getContacts = (request, response) => {
 const getContactById = (request, response) => {
   const id = parseInt(request.params.id)
 
-  pool.query('SELECT * FROM contacts WHERE _id = $1', [id], (error, results) => {
+  pool.query('SELECT * FROM contactsdb WHERE _id = $1', [id], (error, results) => {
     if(error) {
       throw error
     }
@@ -36,7 +36,7 @@ const getContactById = (request, response) => {
 const addContact = (request, response) => {
   const { firstname, lastname, address, emailaddress, contactnumber} = request.body
 
-  pool.query('INSERT INTO contacts (first_name, last_name, address, email_address, contact_number) VALUES ($1, $2, $3, $4, $5)', 
+  pool.query('INSERT INTO contactsdb (first_name, last_name, address, email_address, contact_number) VALUES ($1, $2, $3, $4, $5)', 
     [firstname, lastname, address, emailaddress, contactnumber], 
     (error, results) => {
       if(error){
@@ -51,7 +51,7 @@ const updateContact = (request, response) => {
   const { firstname, lastname, address, emailaddress, contactnumber} = request.body
 
   pool.query(
-    'UPDATE contacts SET first_name = $1, last_name = $2, address = $3, email_address = $4, contact_number = $5 WHERE _id = $6',
+    'UPDATE contactsdb SET first_name = $1, last_name = $2, address = $3, email_address = $4, contact_number = $5 WHERE _id = $6',
     [firstname, lastname, address, emailaddress, contactnumber, id], (error, results) => {
       if(error){
         throw error
@@ -64,7 +64,7 @@ const updateContact = (request, response) => {
 const deleteContact = (request, response) => {
   const id = parseInt(request.params.id)
 
-  pool.query('DELETE FROM contacts WHERE _id = $1', [id], (error, results) => {
+  pool.query('DELETE FROM contactsdb WHERE _id = $1', [id], (error, results) => {
     if(error){
       throw error
     }
